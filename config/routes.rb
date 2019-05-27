bc9b4049fde57c1d5d4599576d104da6be8c5a33
Johnny Chan <johchan24@gmail.com>
2019-05-27 21:52:53 +1000

    fixed routes for user information

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] do
+    resources :users, only: [:first_name, :last_name, :city]
+  end
   resources :bookings
 
   root to: 'items#index'

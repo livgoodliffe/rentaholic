8927699f25140f840c9fbd44107bce01cb57eda7
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-05-27 16:36:51 +1000

    added cetegories to items model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
+  CATEGORIES = ['Technology', 'Sporting Goods', 'Baby & Children', 'Home & Garden', 'Fashion', 'Vehicles', 'Office', 'Games', 'Furniture']
+
   belongs_to :user
 end

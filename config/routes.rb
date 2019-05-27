b2262d0715ca1ad1c15983aadf24ae01f3c0586e
Johnny Chan <johchan24@gmail.com>
2019-05-27 17:32:25 +1000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
   devise_for :users
+
+  resources :items
+  resources :bookings, except: :index
 
   root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

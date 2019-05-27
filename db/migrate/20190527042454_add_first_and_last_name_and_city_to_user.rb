0ce55925263b358a2572621f8e90db65d573b2d3
Joel James <jayfiled@gmail.com>
2019-05-27 14:29:19 +1000

    add first last city to user model

---
+++ db/migrate/20190527042454_add_first_and_last_name_and_city_to_user.rb
@@ -0,0 +1,7 @@
+class AddFirstAndLastNameAndCityToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :city, :string
+  end
+end

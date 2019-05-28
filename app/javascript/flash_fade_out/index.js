30facd8bab4f286b0af1efefaaac45155b3dbbac
Daniel Troy <daniel_j_troy@hotmail.com>
2019-05-28 18:56:04 +1000

    remove unnecessary console logs

--- app/javascript/flash_fade_out/index.js
+++ app/javascript/flash_fade_out/index.js
@@ -3,29 +3,19 @@ export default () => {
 
   const flashAlerts = document.querySelectorAll('.alert')
 
-  console.log(flashAlerts)
-
   const setIntervalLimited = (callback, interval, x) => {
     for (var i = 0; i < x; i++) {
       setTimeout(callback, i * interval);
     }
   }
 
-  // setIntervalLimited(function() {
-  //   console.log('hit');
-  // }, 1000, 10)
-
   const opacity_reducer = () => {
-    console.log('opacity reducer')
-    console.log(flashAlerts)
     flashAlerts.forEach((alert) => {
-      console.log(alert.style.opacity);
       let opacity = ""
       if (alert.style.opacity === "") opacity = "1"
       else {
         opacity = alert.style.opacity;
       }
-      console.log(parseInt(opacity))
       const newOpacity = parseFloat(opacity) - 0.125;
       alert.style.opacity = `${newOpacity}`;
       if (newOpacity === 0) {

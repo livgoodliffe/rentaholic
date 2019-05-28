842aa3b39ee2c6e26779fa7ecd89f446f79e9200
Joel James <jayfiled@gmail.com>
2019-05-28 16:47:38 +1000

    finish dashboard only future bookings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,6 @@ import "bootstrap";
 
 import booking_form from 'booking_form';
 
-const body = document.querySelector('body');
-
-const backWithBackspace = (event) => {
-  event.preventDefault();
-  if (event.keyCode === 8) {
-    window.history.back();
-  }
-}
-
-body.addEventListener('keyup', backWithBackspace);
-
-
 const modal_button = document.getElementById('modal_button')
 
 if (modal_button !== null) {

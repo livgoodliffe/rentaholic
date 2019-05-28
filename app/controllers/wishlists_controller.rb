0c2483982738404c2b95085902b7eea625f8530b
Johnny Chan <johchan24@gmail.com>
2019-05-28 14:54:25 +1000

    add item to wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -9,4 +9,18 @@ class WishlistsController < ApplicationController
 
     redirect_to wishlists_path
   end
+
+  def new
+
+    @wishlist = Wishlist.new
+    create
+  end
+
+  def create
+    @wishlist.user = current_user
+    @wishlist.item = Item.find(params[:item])
+    @wishlist.save
+    # byebug
+    redirect_to wishlists_path
+  end
 end

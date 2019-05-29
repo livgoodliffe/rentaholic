1558f6810d1c9d9eb83e94cc70db8c1282fa7629
Joel James <jayfiled@gmail.com>
2019-05-29 15:50:18 +1000

    create dashboard for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: :show
+  resources :users, only: [:index, :show]
+
+  resource :dashboard, only: :show
 
   resources :items, only: [:index, :show]
 

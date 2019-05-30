09ada0818eea956579a5d7d758354a7580d7ff2d
Joel James <jayfiled@gmail.com>
2019-05-30 10:07:53 +1000

    add all partials

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,16 @@
 class DashboardsController < ApplicationController
   def show
-    byebug
+    # byebug
+
+    # if params[:booking_type] == 'current'
+    # get all bookings that have a start date before today
+    # and an end date after or including today
+    # save to @dash_view, use in the view
+    @bookings = current_user.bookings
+
+    @current_user = current_user
+  # end
+
+
   end
 end
